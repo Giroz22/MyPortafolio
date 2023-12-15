536e37cfa051fd7731365be9cbcd9188bd0b314b
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2023-12-15 11:54:15 -0500

    Proyectos

--- js/proyectos.js
+++ js/proyectos.js
@@ -36,16 +36,15 @@ function mostraProyectos() {
     contenedorProyectos.innerHTML += `
 
         <div class="col">
-                <div class="card h-100">
-                    <a href=${proyecto.url}>
-                        <img src="${proyecto.imagen}" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">${proyecto.nombre}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
+            <div class="card h-100">
+                <a href=${proyecto.url}>
+                    <img src="${proyecto.imagen}" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">${proyecto.nombre}</h5>
+                    <p class="card-text">${proyecto.descripcion}</p>
                 </div>
-            
+            </div>  
         </div>
     `;
   });
