5375a56d763f43a35bfc6711c48fa942e07b4ce3
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-10 12:11:26 -0500

    Base project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryDark: "#0a1128",
+        secontaryDark: "#001f54",
+        tertiaryDark: "#0c4592",
+        primaryLight: "#3b82f6",
+        secontaryLight: "#d9d9d9",
+      },
+    },
+  },
+  plugins: [],
+};
