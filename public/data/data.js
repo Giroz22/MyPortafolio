ede3db9690e119d52ba165c9c8db4db80c36419e
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-11 17:36:06 -0500

    Projects

---
+++ public/data/data.js
@@ -0,0 +1,34 @@
+export const listProjects = [
+  {
+    title: "Restaurante1",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium minus id suscipit enim corrupti? Rem quaerat, excepturi expedita sequi, eius modi veniam tempore unde perferendis architecto similique! Nisi, expedita amet?",
+    technologies: ["react", "js"],
+    urlImg:
+      "https://imgs.search.brave.com/qmSUMmwBNb5V2eAtA61laqdZFRMsQ6lTkPay8OxeqAw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZXVyb2lubm92YS5l/ZHUuZXMvaW1nL3N1/YmlkYXNFZGl0b3Iv/cHVibGlycmVwb3J0/YWplcyUyMCg3KS0x/NjI2MTY3MjQ5Lndl/YnA",
+  },
+  {
+    title: "Restaurante2",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium minus id suscipit enim corrupti? Rem quaerat, excepturi expedita sequi, eius modi veniam tempore unde perferendis architecto similique! Nisi, expedita amet?",
+    technologies: ["react", "js"],
+    urlImg:
+      "https://imgs.search.brave.com/qmSUMmwBNb5V2eAtA61laqdZFRMsQ6lTkPay8OxeqAw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZXVyb2lubm92YS5l/ZHUuZXMvaW1nL3N1/YmlkYXNFZGl0b3Iv/cHVibGlycmVwb3J0/YWplcyUyMCg3KS0x/NjI2MTY3MjQ5Lndl/YnA",
+  },
+  {
+    title: "Restaurante3",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium minus id suscipit enim corrupti? Rem quaerat, excepturi expedita sequi, eius modi veniam tempore unde perferendis architecto similique! Nisi, expedita amet?",
+    technologies: ["react", "js"],
+    urlImg:
+      "https://imgs.search.brave.com/qmSUMmwBNb5V2eAtA61laqdZFRMsQ6lTkPay8OxeqAw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZXVyb2lubm92YS5l/ZHUuZXMvaW1nL3N1/YmlkYXNFZGl0b3Iv/cHVibGlycmVwb3J0/YWplcyUyMCg3KS0x/NjI2MTY3MjQ5Lndl/YnA",
+  },
+  {
+    title: "Restaurante4",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium minus id suscipit enim corrupti? Rem quaerat, excepturi expedita sequi, eius modi veniam tempore unde perferendis architecto similique! Nisi, expedita amet?",
+    technologies: ["react", "js"],
+    urlImg:
+      "https://imgs.search.brave.com/qmSUMmwBNb5V2eAtA61laqdZFRMsQ6lTkPay8OxeqAw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZXVyb2lubm92YS5l/ZHUuZXMvaW1nL3N1/YmlkYXNFZGl0b3Iv/cHVibGlycmVwb3J0/YWplcyUyMCg3KS0x/NjI2MTY3MjQ5Lndl/YnA",
+  },
+];
