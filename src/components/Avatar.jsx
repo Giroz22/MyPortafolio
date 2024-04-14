0e3e1229b894c5f8a650c2ce23b6075969d3ebe1
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-14 17:18:09 -0500

    Avatar updated

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,27 @@
+import "atropos/css";
+
+// import Atropos component
+import Atropos from "atropos/react";
+
 export default function Avatar() {
   return (
-    <div className="h-[350px] w-[350px] rounded-full overflow-hidden">
-      <img
-        class="object-center object-cover"
-        src="../../public/imgs/avatar.jpg"
-        alt=""
-      ></img>
+    <div id="app" className="">
+      <Atropos
+        className="my-atropos w-[360px] h-[360px] relative rounded-full overflow-hidden bg-cover bg-black"
+        shadow={false}
+        highlight={false}
+      >
+        <img
+          src="../../public/imgs/avatar/imagen-bg.jpg"
+          className="absolute w-full h-full"
+          data-atropos-offset="-5"
+        />
+        <img
+          src="../../public/imgs/avatar/image-middle.png"
+          className="absolute object-fill"
+          data-atropos-offset="0"
+        />
+      </Atropos>
     </div>
   );
 }
