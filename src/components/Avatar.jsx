f60e85320031509e716e92765a6febbb63ff664f
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-14 15:14:42 -0500

    Avatar added

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+export default function Avatar() {
+  return (
+    <div className="h-[350px] w-[350px] rounded-full overflow-hidden">
+      <img
+        class="object-center object-cover"
+        src="../../public/imgs/avatar.jpg"
+        alt=""
+      ></img>
+    </div>
+  );
+}
