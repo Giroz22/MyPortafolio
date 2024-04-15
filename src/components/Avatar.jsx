b495792fa4b9c8e8fe5180b90de5f61bb9172ae1
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-15 08:22:08 -0500

    Avatar updated

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,28 +5,30 @@ import Atropos from "atropos/react";
 
 export default function Avatar() {
   return (
-    <div id="app" className="rounded-full overflow-hidden">
-      <Atropos
-        className="my-atropos w-[360px] h-[360px] relative"
-        shadow={false}
-        highlight={false}
-      >
-        <img
-          src="../../public/imgs/avatar/imagen-bg.jpg"
-          className="absolute bottom-0 w-full h-full rounded-full overflow-hidden"
-          data-atropos-offset="-3"
-        />
-        <img
-          src="../../public/imgs/avatar/tierra.png"
-          className="absolute bottom-0 w-full h-full"
-          data-atropos-offset="-1"
-        />
-        <img
-          src="../../public/imgs/avatar/image-middle.png"
-          className="absolute"
-          data-atropos-offset="0"
-        />
-      </Atropos>
+    <div id="app" className="basis-1/2 flex justify-center">
+      <div className="rounded-full overflow-hidden w-[340px] h-[340px]">
+        <Atropos
+          className="my-atropos w-full h-full relative"
+          shadow={false}
+          highlight={false}
+        >
+          <img
+            src="../../public/imgs/avatar/imagen-bg.jpg"
+            className="absolute bottom-0 w-full h-full rounded-full overflow-hidden"
+            data-atropos-offset="-5"
+          />
+          <img
+            src="../../public/imgs/avatar/tierra.png"
+            className="absolute bottom-0 w-full h-full"
+            data-atropos-offset="-3"
+          />
+          <img
+            src="../../public/imgs/avatar/image-middle.png"
+            className="absolute"
+            data-atropos-offset="0"
+          />
+        </Atropos>
+      </div>
     </div>
   );
 }
