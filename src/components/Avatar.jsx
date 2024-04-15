78f64f6952ddbc0d55797c9bb3524330aae72c8d
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-04-15 07:44:27 -0500

    Avatar modified

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,20 +5,25 @@ import Atropos from "atropos/react";
 
 export default function Avatar() {
   return (
-    <div id="app" className="">
+    <div id="app" className="rounded-full overflow-hidden">
       <Atropos
-        className="my-atropos w-[360px] h-[360px] relative rounded-full overflow-hidden bg-cover bg-black"
+        className="my-atropos w-[360px] h-[360px] relative"
         shadow={false}
         highlight={false}
       >
         <img
           src="../../public/imgs/avatar/imagen-bg.jpg"
-          className="absolute w-full h-full"
-          data-atropos-offset="-5"
+          className="absolute bottom-0 w-full h-full rounded-full overflow-hidden"
+          data-atropos-offset="-3"
+        />
+        <img
+          src="../../public/imgs/avatar/tierra.png"
+          className="absolute bottom-0 w-full h-full"
+          data-atropos-offset="-1"
         />
         <img
           src="../../public/imgs/avatar/image-middle.png"
-          className="absolute object-fill"
+          className="absolute"
           data-atropos-offset="0"
         />
       </Atropos>
